45531d6f1b9137e54d54a751a5d4bcc7b074c421
sumitshukla2000 <shuklasumit93@gmail.com>
2023-10-06 00:48:57 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily:{
+      'main' : ['Young Serif'],
+      'primary':['Roboto'],
+      'avg':['Gabarito']
+    },
+    screens:{
+      'sm':'540px',
+      'md':'768px',
+      'lg':'1024px',
+      'xs':'1280'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
